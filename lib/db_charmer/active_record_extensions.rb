689be3dbbf1ca1b7cc5b5424a96c41b2805fecd0
Alexey Kovyrin <alexey@kovyrin.net>
2009-08-12 16:46:09 -0400

    Implemented db_magic :connection => :foo method and tested it. Started implementing slaves management.

--- lib/db_charmer/active_record_extensions.rb
+++ lib/db_charmer/active_record_extensions.rb
@@ -1,6 +1,7 @@
 module DbCharmer
   module ActiveRecordExtensions
     module ClassMethods
+
       def establish_real_connection_if_exists(name, should_exist = false)
         config = configurations[RAILS_ENV][name.to_s]
         if should_exist && !config
@@ -9,6 +10,7 @@ module DbCharmer
         establish_connection(config) if config
       end
       
+      #-----------------------------------------------------------------------------
       @@db_charmer_connection_proxies = {}
       def db_charmer_connection_proxy=(proxy)
         @@db_charmer_connection_proxies[self.to_s] = proxy
@@ -18,6 +20,17 @@ module DbCharmer
         @@db_charmer_connection_proxies[self.to_s]
       end
       
+      #-----------------------------------------------------------------------------
+      @@db_charmer_slaves = {}
+      def db_charmer_slaves=(slaves)
+        @@db_charmer_slaves[self.to_s] = slaves
+      end
+
+      def db_charmer_slaves
+        @@db_charmer_slaves[self.to_s] || []
+      end
+      
+      #-----------------------------------------------------------------------------
       def hijack_connection!
         puts "DEBUG: Hijacking connection for #{self.to_s}"
         class << self 
@@ -26,6 +39,7 @@ module DbCharmer
           end
         end
       end
+      
     end
   end
 end
--- lib/db_charmer/db_magic.rb
+++ lib/db_charmer/db_magic.rb
@@ -1,8 +1,32 @@
 module DbCharmer
   module DbMagic
     module ClassMethods
-      def db_magic(opt)
-        # FIXME: Do the magic here
+      def db_magic(opt = {})
+        # Make sure we could use our connections management here
+        hijack_connection!
+        
+        # Should requested connections exist in the config?
+        should_exist = opt[:should_exist] || DbCharmer.connections_should_exist?
+        
+        # Main connection management
+        db_magic_connection(opt[:connection], should_exist) if opt[:connection]
+
+        # Set up slaves pool
+        opt[:slaves] ||= []
+        opt[:slaves] << opt[:slave] if opt[:slave]
+        db_magic_slaves(opt[:slaves], should_exist)
+      end
+
+    private
+   
+      def db_magic_connection(conn, should_exist = false)
+        switch_connection_to(conn, should_exist)
+      end
+      
+      def db_magic_slaves(slaves, should_exist = false)
+        self.db_charmer_slaves = slaves.collect do |slave|
+          DbCharmer::ConnectionFactory.connect(slave, should_exist)
+        end
       end
     end
   end
