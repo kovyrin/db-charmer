02ecba4b8b08a12d46dfe9d528ee1875f764cbec
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-04 14:40:08 -0400

    Use class as a key for connections hashes, not the class.to_s

--- lib/db_charmer/active_record_extensions.rb
+++ lib/db_charmer/active_record_extensions.rb
@@ -13,31 +13,31 @@ module DbCharmer
       #-----------------------------------------------------------------------------
       @@db_charmer_opts = {}
       def db_charmer_opts=(opts)
-        @@db_charmer_opts[self.to_s] = opts
+        @@db_charmer_opts[self] = opts
       end
 
       def db_charmer_opts
-        @@db_charmer_opts[self.to_s] || {}
+        @@db_charmer_opts[self] || {}
       end
 
       #-----------------------------------------------------------------------------
       @@db_charmer_connection_proxies = {}
       def db_charmer_connection_proxy=(proxy)
-        @@db_charmer_connection_proxies[self.to_s] = proxy
+        @@db_charmer_connection_proxies[self] = proxy
       end
 
       def db_charmer_connection_proxy
-        @@db_charmer_connection_proxies[self.to_s]
+        @@db_charmer_connection_proxies[self]
       end
 
       #-----------------------------------------------------------------------------
       @@db_charmer_slaves = {}
       def db_charmer_slaves=(slaves)
-        @@db_charmer_slaves[self.to_s] = slaves
+        @@db_charmer_slaves[self] = slaves
       end
 
       def db_charmer_slaves
-        @@db_charmer_slaves[self.to_s] || []
+        @@db_charmer_slaves[self] || []
       end
 
       def db_charmer_random_slave
@@ -48,11 +48,11 @@ module DbCharmer
       #-----------------------------------------------------------------------------
       @@db_charmer_connection_levels = Hash.new(0)
       def db_charmer_connection_level=(level)
-        @@db_charmer_connection_levels[self.to_s] = level
+        @@db_charmer_connection_levels[self] = level
       end
 
       def db_charmer_connection_level
-        @@db_charmer_connection_levels[self.to_s] || 0
+        @@db_charmer_connection_levels[self] || 0
       end
 
       def db_charmer_top_level_connection?
