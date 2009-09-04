6dd4965a52137801579c3086e47866a9bcbb1c7e
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-03 22:10:07 -0400

    Removed too detailed logging

--- lib/db_charmer/connection_proxy.rb
+++ lib/db_charmer/connection_proxy.rb
@@ -5,7 +5,6 @@ module DbCharmer
     end
 
     def method_missing(meth, *args, &block)
-      DbCharmer.logger.debug("Proxying #{meth} call to #{@abstract_connection_class}")
       @abstract_connection_class.retrieve_connection.send(meth, *args, &block)
     end
   end
