caa0318284f940213976167cea9e600e92a869b6
Oleksiy Kovyrin <scoundrel@Oleksiy-Kovyrins-MacBook.local>
2009-09-03 20:17:07 -0600

    Fixed dependencies loading

--- lib/db_charmer.rb
+++ lib/db_charmer.rb
@@ -1,3 +1,5 @@
+puts "Loading DbCharmer..."
+
 module DbCharmer
   @@migration_connections_should_exist = Rails.env.production?
   mattr_accessor :migration_connections_should_exist
@@ -14,19 +16,17 @@ module DbCharmer
   end
   
   def self.logger
-    return Rails.logger if defined?(Rails)
+    #return Rails.logger if defined?(Rails)
     @logger ||= Logger.new(STDERR)
   end
 end
+
+puts "Extending AR..."
 
 require 'db_charmer/active_record_extensions'
 require 'db_charmer/connection_factory'
 require 'db_charmer/connection_proxy'
 require 'db_charmer/connection_switch'
-require 'db_charmer/db_magic'
-require 'db_charmer/finder_overrides'
-require 'db_charmer/multi_db_migrations'
-require 'db_charmer/multi_db_proxy'
 
 # Enable misc AR extensions
 ActiveRecord::Base.extend(DbCharmer::ActiveRecordExtensions::ClassMethods)
@@ -36,6 +36,14 @@ ActiveRecord::Base.extend(DbCharmer::ConnectionSwitch::ClassMethods)
 
 # Enable connection proxy in AR
 ActiveRecord::Base.extend(DbCharmer::MultiDbProxy::ClassMethods)
+ActiveRecord::Base.send(:include, DbCharmer::MultiDbProxy::InstanceMethods)
+
+puts "Doing the magic..."
+
+require 'db_charmer/db_magic'
+require 'db_charmer/finder_overrides'
+require 'db_charmer/multi_db_migrations'
+require 'db_charmer/multi_db_proxy'
 
 # Enable multi-db migrations
 ActiveRecord::Migration.extend(DbCharmer::MultiDbMigrations::ClassMethods)
