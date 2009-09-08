5965f86126791930e0217da8e5b0ed1c9361945e
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-08 17:44:09 -0400

    Add on_master/on_slave methods to the association proxy

--- lib/db_charmer/association_proxy.rb
+++ lib/db_charmer/association_proxy.rb
@@ -4,7 +4,14 @@ module DbCharmer
       def on_db(con, &block)
         @reflection.klass.on_db(con, self, &block)
       end
+
+      def on_slave(con = nil, &block)
+        @reflection.klass.on_slave(con, self, &block)
+      end
+
+      def on_master(&block)
+        @reflection.klass.on_master(self, &block)
+      end
     end
   end
 end
-
