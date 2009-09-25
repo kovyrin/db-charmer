8d28e8a94f9af448ce2ebb2d83f8a05cf6a0a84c
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-25 10:57:22 -0700

    Implemented and tested Range sharding method

---
+++ lib/db_charmer/sharding/method/range.rb
@@ -0,0 +1,23 @@
+module DbCharmer
+  module Sharding
+    module Method
+      class Range
+        attr_accessor :ranges
+        
+        def initialize(config)
+          @ranges = config[:ranges] or raise ArgumentError, "No :ranges defined!"
+        end
+
+        def shard_for_key(key)
+          ranges.each do |range, shard|
+            next if range == :default
+            return shard if range.member?(key.to_i)
+          end
+          return ranges[:default] if ranges[:default]
+          raise ArgumentError, "Invalid key value, no shards found for this key!"
+        end
+        
+      end
+    end
+  end
+end
