0f73787bef94fd23a38e9fcefb09a6e790fbdee5
Alexey Kovyrin <alexey@kovyrin.net>
2010-04-07 01:44:23 -0400

    Change the way we hadle associations preloads: do not switch connections when not needed

--- lib/db_charmer/association_preload.rb
+++ lib/db_charmer/association_preload.rb
@@ -5,7 +5,9 @@ module DbCharmer
       ASSOCIATION_TYPES.each do |association_type|
         class_eval <<-EOF, __FILE__, __LINE__ + 1
           def preload_#{association_type}_association(records, reflection, preload_options = {})
-            return super(records, reflection, preload_options) if self.db_charmer_top_level_connection?
+            if self.db_charmer_top_level_connection? || self.db_charmer_default_connection != reflection.klass.db_charmer_default_connection
+              return super(records, reflection, preload_options)
+            end
             reflection.klass.on_db(self) do
               super(records, reflection, preload_options)
             end
