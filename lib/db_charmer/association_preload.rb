51d8c1634772ba7592d1e7d38f5365c0272e7733
Alexey Kovyrin <alexey@kovyrin.net>
2010-04-08 12:29:22 -0400

    Cleanup associal preload overrides

--- lib/db_charmer/association_preload.rb
+++ lib/db_charmer/association_preload.rb
@@ -2,17 +2,20 @@ module DbCharmer
   module AssociationPreload
     module ClassMethods
       ASSOCIATION_TYPES = [ :has_one, :has_many, :belongs_to, :has_and_belongs_to_many ]
-      ASSOCIATION_TYPES.each do |association_type|
-        class_eval <<-EOF, __FILE__, __LINE__ + 1
-          def preload_#{association_type}_association(records, reflection, preload_options = {})
-            if self.db_charmer_top_level_connection? || self.db_charmer_default_connection != reflection.klass.db_charmer_default_connection
-              return super(records, reflection, preload_options)
+
+      def self.extended(base)
+        ASSOCIATION_TYPES.each do |association_type|
+          base.class_eval <<-EOF, __FILE__, __LINE__ + 1
+            def self.preload_#{association_type}_association(records, reflection, preload_options = {})
+              if self.db_charmer_top_level_connection? || self.db_charmer_default_connection != reflection.klass.db_charmer_default_connection
+                return super(records, reflection, preload_options)
+              end
+              reflection.klass.on_db(self) do
+                super(records, reflection, preload_options)
+              end
             end
-            reflection.klass.on_db(self) do
-              super(records, reflection, preload_options)
-            end
-          end
-        EOF
+          EOF
+        end
       end
     end
   end
