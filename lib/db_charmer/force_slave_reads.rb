4db2d9fdbdd6e15e83776c0deb6990f5d11cdde3
Alexey Kovyrin <alexey@kovyrin.net>
2011-05-16 00:54:27 -0400

    Added DbCharmer.force_slave_reads method

--- lib/db_charmer/force_slave_reads.rb
+++ lib/db_charmer/force_slave_reads.rb
@@ -2,8 +2,15 @@ module DbCharmer
   @@current_controller = nil
   mattr_accessor :current_controller
 
+  @@forced_slave_reads = false
+
   def self.force_slave_reads?
+    # If global force slave reads is requested, do it
+    return @@forced_slave_reads if @@forced_slave_reads
+
+    # If not, try to use current controller to decide on this
     return false unless current_controller.respond_to?(:force_slave_reads?)
+
     slave_reads = current_controller.force_slave_reads?
     logger.debug("Using controller to figure out if slave reads should be forced: #{slave_reads}")
     return slave_reads
@@ -17,5 +24,13 @@ module DbCharmer
   ensure
     logger.debug('Clearing current controller for db_charmer')
     self.current_controller = nil
+  end
+
+  def self.force_slave_reads
+    raise ArgumentError, "No block given" unless block_given?
+    @@forced_slave_reads = true
+    yield
+  ensure
+    @@forced_slave_reads = false
   end
 end
