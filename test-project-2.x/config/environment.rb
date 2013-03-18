47aaea6150b93cc7f6713d96d2ef1f2343f3958b
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-03-18 14:57:23 -0400

    doh

--- test-project-2.x/config/environment.rb
+++ test-project-2.x/config/environment.rb
@@ -1,5 +1,5 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
